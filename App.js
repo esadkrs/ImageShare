14c1572e5e05952fbd652ef5fd4d7b3a1ec32c26
esadkrs <esadkiris@gmail.com>
2021-04-22 02:27:43 -0400

    logo

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={{ width: 305, height: 159 }} /> 
+      
+      <Text style={{color: '#00008b', fontSize: 21}}>
+        To share a photo from your phone with a friend, just press the buton below!</Text>
       <StatusBar style="auto" />
     </View>
   );
